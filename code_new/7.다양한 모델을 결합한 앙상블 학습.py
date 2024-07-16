875470b1ac3e527d97750a4469834c381f6b86f8
Akai_LG_gram16 <akaiuun12@gmail.com>
2024-07-16 21:51:54 +0900

    updated

---
+++ code_new/7.다양한 모델을 결합한 앙상블 학습.py
@@ -0,0 +1,106 @@
+# %% Chapter 7. 다양한 모델을 결합한 앙상블 학습
+import numpy as np
+
+# 7.2.1 간단한 다수결 투표 분류기 구현하기
+print(np.argmax(np.bincount([0, 0, 1], weights=[0.2, 0.2, 0.6])))
+
+ex = np.array([[0.9, 0.1],
+               [0.8, 0.2],
+               [0.4, 0.6]])
+p = np.average(ex, axis=0, weights=[0.2, 0.2, 0.6])
+print(np.argmax(p))
+
+# %% 7.2.2 다수결 투표 방식을 사용하여 예측 만들기
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+iris = datasets.load_iris()
+X, y = iris.data[50:, [1, 2]], iris.target[50:]
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=1, stratify=y)
+
+# %% 3개의 분류기(Logistic Regression, DT, kNN)를 훈련하고 평가하기
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+
+clf1 = LogisticRegression(solver='liblinear', penalty='l2', C=0.001, random_state=1)
+clf2 = DecisionTreeClassifier(max_depth=1, criterion='entropy', random_state=0)
+clf3 = KNeighborsClassifier(n_neighbors=1, p=2, metric='minkowski')
+
+pipe1 = Pipeline([['sc', StandardScaler()], ['clf', clf1]])
+pipe3 = Pipeline([['sc', StandardScaler()], ['clf', clf3]])
+
+clf_labels = ['Logistic regression', 'Decision tree', 'KNN']
+print('10-겹 교차 검증:\n')
+for clf, label in zip([pipe1, clf2, pipe3], clf_labels):
+    scores = cross_val_score(estimator=clf, X=X_train, y=y_train, cv=10, scoring='roc_auc')
+    print(f'ROC AUC: {scores.mean():.2f} (+/- {scores.std():.2f}) [{label}]')
+
+# %% 3개의 분류기를 Majority Voting을 사용하여 연결하기
+from sklearn.ensemble import VotingClassifier
+mv_clf = VotingClassifier(estimators=[('lr', pipe1), ('dt', clf2), ('knn', pipe3)], voting='soft')
+
+all_clf = [pipe1, clf2, pipe3, mv_clf]
+clf_labels += ['Majority voting']
+
+print('10-겹 교차 검증:\n')
+
+for clf, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=clf, X=X_train, y=y_train, cv=10, scoring='roc_auc')
+    print(f'ROC AUC: {scores.mean():.2f} (+/- {scores.std():.2f}) [{label}]')
+
+# %% 7.2.3 앙상블 분류기의 평가와 튜닝
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+
+colors = ['black', 'orange', 'blue', 'green']
+linestyles = [':', '--', '-.', '-']
+
+for clf, label, clr, ls in zip(all_clf, clf_labels, colors, linestyles):
+    y_pred = clf.fit(X_train, y_train).predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
+    roc_auc = auc(x=fpr, y=tpr)
+    plt.plot(fpr, tpr, color=clr, linestyle=ls, label=f'{label} (auc = {roc_auc:.2f})')
+
+plt.legend(loc='lower right')
+plt.plot([0, 1], [0, 1], linestyle='--', color='gray', linewidth=2)
+plt.xlim([-0.1, 1.1])
+plt.ylim([-0.1, 1.1])
+plt.grid(alpha=0.5)
+plt.xlabel('False positive rate (FPR)')
+plt.ylabel('True positive rate (TPR)')
+plt.show()
+
+# %% 결정 경계 비교
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+
+from itertools import product
+x_min = X_train_std[:, 0].min() - 1
+x_max = X_train_std[:, 0].max() + 1
+y_min = X_train_std[:, 1].min() - 1
+y_max = X_train_std[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
+
+f, axarr = plt.subplots(nrows=2, ncols=2, sharex='col', sharey='row', figsize=(7, 5))
+
+for idx, clf, tt in zip(product([0, 1], [0, 1]), all_clf, clf_labels):
+    clf.fit(X_train_std, y_train)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx[0], idx[1]].scatter(X_train_std[y_train == 0, 0], X_train_std[y_train == 0, 1], c='blue', marker='^')
+    axarr[idx[0], idx[1]].scatter(X_train_std[y_train == 1, 0], X_train_std[y_train == 1, 1], c='green', marker='o')
+    axarr[idx[0], idx[1]].set_title(tt)
+
+plt.text(-3.5, -5.5, s='Sepal width [standardized]', ha='center', va='center', fontsize=12)
+plt.text(-12.5, 4.5, s='Petal length [standardized]', ha='center', va='center', fontsize=12, rotation=90)
+plt.show()
